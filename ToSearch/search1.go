989d2b4837e223aa87b0095a77906f726fdc8ac3
lukas <82029186+42lukas@users.noreply.github.com>
2025-11-19 13:47:14 +0100

    added "TODO" pattern to file

--- ToSearch/search1.go
+++ ToSearch/search1.go
@@ -6,3 +6,13 @@ func main() {
     // TODO: handle CLI flags
     fmt.Println("Hello")
 }
+
+// TODO: implement addition function
+func add(a int, b int) int {
+    return a + b
+}
+
+// TODO: implement multiplication function
+func multiply(a int, b int) int {
+    return a * b
+}
